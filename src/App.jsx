b3f3ad573f4de3cba0bd2f53f74e3c2a6895fdab
MIM-Education <zakwan@mim.org.my>
2025-10-24 11:41:30 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -766,7 +766,8 @@ const AdminDashboard = () => {
                             : selectedSubmission.status === 'registered'
                             ? 'bg-green-100 text-green-800'
                             : selectedSubmission.status === 'attended'
-                            ? 'bg-blue-100 text-blue-800'                            
+                            ? 'bg-blue-100 text-blue-800'
+                            : 'bg-red-100 text-red-800'
                       }`}>
                         {selectedSubmission.status}
                       </span>
